dbafd941db9ef488a770691cfea49d403d0f5c5e
Catlinman <andrew.lake@amwasser.de>
2014-08-17 12:20:44 +0200

    Added album downloading

--- campdown.py
+++ campdown.py
@@ -19,17 +19,16 @@ try:
     if outputfolder[-1] != "/":
         outputfolder = str(outputfolder) + "/"
 except:
-    outputfolder = os.path.dirname(__file__) + "/"
+    outputfolder = os.path.dirname(__file__).replace('\\', '/') + "/"
 
 bandcamp_base_url = str(bandcamp_track_url).split(".com", 1)[0] + ".com"
 bandcamp_album = ""
 
 r = requests.get(bandcamp_track_url).content
-print(r)
 full_json = []
 
 if str(r).find("track_list") == -1:
-    raw_json = ("{" + (str(r).split("trackinfo: [{",1)[1].split("}]")[0]) + "} ")
+    raw_json = ("{" + (str(r).split("trackinfo: [{",1)[1].split("}]")[0]) + "} ").replace('\\', ' ')
     track_json = json.loads(raw_json)
     full_json.insert(1, track_json)
 
@@ -40,11 +39,34 @@ else:
         print('Created album folder in %s' % (outputfolder + "/" + bandcamp_album + "/"))
         os.makedirs(outputfolder + "/" + bandcamp_album + "/")
 
-    # Split up each track's link and retrieve the json from the given track's page.
+    bandcamp_album_tracktable = str(r).split('<table class="track_list track_table" id="track_table">',1)[1].split('</table>')[0]
+    bandcamp_album_tracks = bandcamp_album_tracktable.split("<tr")
+
+    print('\nListing found tracks')
+    for i, track in enumerate(bandcamp_album_tracks):
+        position = track.find('<a href="/track')
+        
+        if position != -1:
+            position = position + len('<a href="/track')
+            trackname = ""
+            while track[position] != '"':
+                trackname = trackname + track[position]
+                position = position + 1
+
+            if trackname != "":
+                print("" + bandcamp_base_url + "/track" + trackname)
+
+                track_r = requests.get(bandcamp_base_url + "/track" + trackname).content
+                raw_json = ("{" + (str(track_r).split("trackinfo: [{",1)[1].split("}]")[0]) + "} ").replace('\\', ' ')
+                track_json = json.loads(raw_json)
+                full_json.insert(i, track_json)
 
 totaltracks = len(full_json)
 
-print('' + "Writing all output files to %s%s" % (outputfolder, bandcamp_album))
+if bandcamp_album != "":
+    outputfolder = outputfolder + bandcamp_album
+
+print("Writing all output files to %s" % outputfolder)
 
 for i in range(0, totaltracks): 
     track = i
@@ -52,14 +74,15 @@ for i in range(0, totaltracks):
     duration = full_json[i]["duration"]
 
     if bandcamp_album != "":
-        partialtitle = str(title).split("-", 1)
-        title = partialtitle[0] + " - " + bandcamp_album + " - " + str(i) + partialtitle[1]
+        if title.find(" - ") != -1:
+            partialtitle = str(title).split("-", 1)
+            title = partialtitle[0] + " - " + bandcamp_album + " - " + str(i) + " " + partialtitle[1]
+        else:
+            title = bandcamp_album + " - " + str(i) + " " + title
 
     fileurl = full_json[i]["file"]["mp3-128"]
 
-    print('\nFetched URL from %s' % fileurl)
-
-    with open(outputfolder + title + ".mp3", "wb") as f:
+    with open(outputfolder + "/" + title + ".mp3", "wb") as f:
 
             print('\n' + "Downloading: " + title)
             response = requests.get(fileurl, stream = True)
